cb308fab9d6403b0c0e8df0492a1b4ce12ba6cfb
Srikanta Pramanik <105414609+Srikanta1122@users.noreply.github.com>
2023-12-02 01:36:57 +0530

    Time: 16 ms (74.14%) | Memory: 44.1 MB (48.98%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,68 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;
+            }
+
+            for(int j=i+1;j<n;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])
+                {
+                    continue;
+                }
+
+                int k=j+1;
+                int l=n-1;
+
+                while(k<l)
+                {
+                   // long sum=nums[i]+nums[j]+nums[k]+nums[l];
+                   long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if(sum<target)
+                    {
+                        k++;
+                    }
+                    else if(sum>target)
+                    {
+                        l--;
+                    }
+                    else
+                    {
+                        
+                    List<Integer> ls=Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+
+
+                    ans.add(ls);
+                    k++;
+                    l--;
+                    //skip the duplicates for 'k' and 'l'
+                    while(k<l && nums[k]==nums[k-1])
+                    {
+                        k++;
+                    }
+
+                    while(k<l && nums[l]==nums[l+1])
+                    {
+                        l--;
+                    }
+            }
+        }
+            }
+        }
+
+        return ans;
+
+    }
+}
+
